988ccf9e09cdcf7e04372ff659218f9d8ebc56e8
unknown <tomeshwarshiw@gmail.com>
2021-11-30 13:57:29 +0530

    Added Initial Checkin

---
+++ rlm/src/main/java/com/rlm/RlmApplication.java
@@ -0,0 +1,13 @@
+package com.rlm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RlmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RlmApplication.class, args);
+	}
+
+}
---
+++ rlm/src/test/java/com/rlm/RlmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rlm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RlmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
